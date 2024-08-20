dab1cd2ff575b2037c09afcab9dc25c181122c8c
Teknicallity <joshua.s.3322@gmail.com>
2024-08-19 23:46:40 -0400

    Fix creating new zip pack

--- internal/zipUtil/zipBepinexFiles.go
+++ internal/zipUtil/zipBepinexFiles.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strconv"
+	"regexp"
 )
 
 // zipFiles zips the specified files into a single zip archive
@@ -47,20 +47,15 @@ func addToZip(archive *zip.Writer, filename string) error {
 		return err
 	}
 
-	// Create a header for the file or directory
-	var header *zip.FileHeader
 	if info.IsDir() {
-		// If it's a directory, use FileInfoHeader to create the header
-		header, err = zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
-	} else {
-		// If it's a file, also use FileInfoHeader
-		header, err = zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
+		// Add the directory itself and recursively add its contents
+		return addDirToZip(archive, filename)
+	}
+
+	// Create a header for the file
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
 	}
 
 	// Change the header name to the relative path
@@ -75,32 +70,74 @@ func addToZip(archive *zip.Writer, filename string) error {
 		return err
 	}
 
-	// If it's a file, copy its content into the zip file
-	if !info.IsDir() {
-		if _, err = io.Copy(writer, fileToZip); err != nil {
-			return err
-		}
+	// Copy the content of the file into the zip file
+	if _, err = io.Copy(writer, fileToZip); err != nil {
+		return err
 	}
 
 	return nil
+}
+
+// addDirToZip adds a directory and its contents to the zip archive
+func addDirToZip(archive *zip.Writer, dirPath string) error {
+	err := filepath.WalkDir(dirPath, func(path string, info os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Compute the relative path of the file
+		relPath, err := filepath.Rel(filepath.Dir(dirPath), path)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			// Add directory with a trailing slash
+			header := &zip.FileHeader{
+				Name:   filepath.ToSlash(relPath) + "/",
+				Method: zip.Store,
+			}
+			_, err := archive.CreateHeader(header)
+			return err
+		} else {
+			// Add file
+			return addToZip(archive, path)
+		}
+	})
+	return err
+}
+
+func getZipPackVersionFromUser() (string, error) {
+	// Define the regex pattern to match a number in the format # or #.#
+	pattern := `^\d+(\.\d+)?$`
+	re := regexp.MustCompile(pattern)
+
+	for {
+		var num string
+		fmt.Printf("What version is this pack (1, 2, etc.): ")
+		_, err := fmt.Scanln(&num)
+		if err != nil {
+			return "", fmt.Errorf("error reading: %v", err)
+		}
+
+		if re.MatchString(num) {
+			return num, nil
+		} else {
+			fmt.Println("Invalid input. Please enter a valid version number.")
+		}
+	}
 }
 
 func ZipBepinEx() error {
 	//Files and directories to be zipped
 	files := []string{"winhttp.dll", "doorstop_config.ini", "BepInEx"}
 
-	var num string
-	fmt.Printf("What version is this pack (1, 2, etc.): ")
-	_, err := fmt.Scanln(&num)
+	version, err := getZipPackVersionFromUser()
 	if err != nil {
-		return fmt.Errorf("error reading: %e", err)
-	}
-	version, err := strconv.Atoi(num)
-	if err != nil {
-		return fmt.Errorf("not an int %e", err)
+		return fmt.Errorf("could not get zip pack version from user: %e", err)
 	}
 
-	zipFileName := fmt.Sprintf("BepinExPack_v%d.zip", version)
+	zipFileName := fmt.Sprintf("BepinExPack_v%s.zip", version)
 
 	// Create zip file
 	err = zipFiles(zipFileName, files)
